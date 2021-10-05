dfcbd505cdb9736fc3ba6cb584055319113340ae
zoneel <40122657+zoneel@users.noreply.github.com>
2021-10-05 11:06:49 +0200

    Initial commit

---
+++ resources/js/clock.js
@@ -0,0 +1,30 @@
+0// ----------------------------------------------------------------- //
+// ------------- ►►► </> with ♥ by Vittorio Retrivi ◄◄◄ ------------ //
+// ----------------------------------------------------------------- //
+
+const clock = document.getElementById('clock');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+
+const randomQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
+
+const setClock = () => {
+  let time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+  clock.innerText = document.title = hours + ":" + minutes + ":" + seconds
+}
+
+const setQuote = () => {
+  let q = randomQuote();
+  quote.innerText = q.quote;
+  author.innerText = q.author;
+}
+
+setQuote();
+setClock();
+setInterval(setClock, 1000);
